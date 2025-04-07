af11935239018c64300f722dc0d3e49a6c8b9156
Geiselle Holt <geiselleholt@hotmail.com>
2025-04-06 18:08:18 -0600

    Part one done except using composition tools

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-function getUserData(id) {
+async function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
-    db3: db3
+    db3: db3,
   };
+
+  try {
+    const dbNumber = await central(id);
+    const dbData = await dbs[dbNumber](id);
+    const vaultData = await vault(id);
+
+    return {
+      id: id,
+      name: vaultData.name,
+      username: dbData.username,
+      email: vaultData.email,
+      address: {
+        street: vaultData.address.street,
+        suite: vaultData.address.suite,
+        city: vaultData.address.city,
+        zipcode: vaultData.address.zipcode,
+        geo: {
+          lat: vaultData.address.geo.lat,
+          lng: vaultData.address.geo.lng,
+        },
+      },
+      phone: vaultData.phone,
+      website: dbData.website,
+      company: {
+        name: dbData.company.name,
+        catchPhrase: dbData.company.catchPhrase,
+        bs: dbData.company.bs,
+      },
+    };
+  } catch (error) {
+    throw new Error(`Failed to get user data: ${error.message}`);
+  }
 }
+
